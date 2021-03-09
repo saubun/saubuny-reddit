9e027ec6d2c77a2f2ae10a7ca560f1a5b7240f0d
saubun <dameon0430@gmail.com>
2021-03-09 14:16:16 -0500

    made backend

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
